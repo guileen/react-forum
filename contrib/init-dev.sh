71a8a2dd091be63a72f204a1ea6b57a46a6c855b
Gui Lin <guileen@gmail.com>
2016-01-21 19:57:47 +0800

    update contribute rules

--- contrib/init-dev.sh
+++ contrib/init-dev.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-ln -s ../../contrib/git/hooks/pre-commit ../.git/hooks/pre-commit
+BASEDIR=$(dirname $0)
+echo "Enable git hooks..."
+ln -sf ../../contrib/git/hooks/pre-commit $BASEDIR/../.git/hooks/pre-commit
+echo "Hooks enabled."
+echo
+echo "Please enable editorconfig plugin in your IDE."
+echo
+echo "Please run below command manually:"
+echo "    npm install -g eslint babel-eslint eslint-plugin-react"
