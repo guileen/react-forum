29abb981367e51b197af9716ef9ebd66fc674bdd
Gui Lin <guileen@gmail.com>
2016-01-24 03:27:04 +0800

    add test case, but not work

--- js/components/Example.js
+++ js/components/Example.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import * as ExampleActions from '../actions/ExampleActions'
-import styles from '../../css/app.css'
+// import styles from '../../css/app.css'
 
 class Example extends Component {
   render() {
+    console.log('calling render')
     const {title, dispatch} = this.props
     const actions = bindActionCreators(ExampleActions, dispatch)
     return (
         <div>
-          <h1 className={styles.text}>Welcome {title}!</h1>
-          <button onClick={e => actions.changeTitle(prompt())}>
+          {/* <h1 className={styles.text}>Welcome {title}!</h1> */}
+          <h1>Welcome {title}!</h1>
+          <button onClick={e => actions.changeTitle('' + Date.now())}>
             Update Title
           </button>
         </div>
