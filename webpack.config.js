dcaf106ca0ae7590128812fbc1be2b51fe7e69f5
Gui Lin <guileen@gmail.com>
2016-01-26 00:02:01 +0800

    update cssnext to postcss

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, loaders: ['babel'], exclude: /node_modules/ },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?module!cssnext-loader') }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?module!postcss-loader') }
     ]
   },
   resolve: {
