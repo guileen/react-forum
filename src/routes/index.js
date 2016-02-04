c5a09bab9bacc273ed5769b8c56eaeff507d4b3e
Gui Lin <guileen@gmail.com>
2016-02-04 17:08:14 +0800

    chore(lint):fix lint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, IndexRoute, Redirect } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 
 // NOTE: here we're making use of the `resolve.root` configuration
 // option in webpack, which allows us to specify import paths as if
